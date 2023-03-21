4b3764fadbbfaf67016cab688f4a1e82423fad42
shnishi <shigeonishi@hotmail.com>
2023-03-21 17:15:11 -0500

    First commit

---
+++ src/main/java/com/_40dev/githubactionsdemo/GithubactionsdemoApplication.java
@@ -0,0 +1,13 @@
+package com._40dev.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_40dev/githubactionsdemo/GithubactionsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._40dev.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
